bf1c0aac13bc142ee8431ce9ed73612b9f760a1c
javipepe <me@javipepe.pw>
2017-03-25 16:44:02 +0100

    Some changes to Dynamic Rotations

--- PGM/src/main/java/tc/oc/pgm/rotation/DynamicRotationChangeListener.java
+++ PGM/src/main/java/tc/oc/pgm/rotation/DynamicRotationChangeListener.java
@@ -1,5 +1,6 @@
 package tc.oc.pgm.rotation;
 
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.TranslatableComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
@@ -7,8 +8,6 @@ import org.bukkit.event.Listener;
 import tc.oc.pgm.PGM;
 import tc.oc.pgm.cycle.CycleMatchModule;
 import tc.oc.pgm.events.MatchEndEvent;
-
-import java.util.Map;
 
 public class DynamicRotationChangeListener implements Listener {
 
@@ -19,29 +18,36 @@ public class DynamicRotationChangeListener implements Listener {
         // Ignore if there is only one rotation available
         if (rotationManager.getRotations().size() == 1) return;
 
+        // Number of players we can assume is active
         int participatingPlayers = event.getMatch().getParticipatingPlayers().size();
-        int bestCandidateCount = Integer.MAX_VALUE;
-        RotationState bestCandidate = null;
 
-        for (Map.Entry<String, RotationState> entry : rotationManager.getRotations().entrySet()) {
-            int playerDifference = Math.abs(entry.getValue().getAverageNeededPlayers() - participatingPlayers);
-            if (playerDifference < bestCandidateCount) {
-                bestCandidateCount = playerDifference;
-                bestCandidate = entry.getValue();
-            }
+        RotationCategory appr = getAppropriateRotationCategory(participatingPlayers, rotationManager);
+        if (appr != null && !appr.toString().toLowerCase().equals(rotationManager.getCurrentRotationName().toLowerCase())) {
+            rotationManager.setRotation(rotationManager.getRotation(appr.toString().toLowerCase()));
+            CycleMatchModule cmm = event.getMatch().needMatchModule(CycleMatchModule.class);
+            cmm.startCountdown(cmm.getConfig().countdown());
+
+            event.getMatch().sendMessage(new TextComponent(ChatColor.RED + "" + ChatColor.STRIKETHROUGH + "-----------------------------------"));
+            event.getMatch().sendMessage(new TextComponent(ChatColor.GOLD + "           " + ChatColor.BOLD + "" + new TranslatableComponent("rotation.change.broadcast.title").getTranslate()));
+            event.getMatch().sendMessage(new TextComponent(ChatColor.YELLOW + "  " + new TranslatableComponent("rotation.change.broadcast.info").getTranslate()));
+            event.getMatch().sendMessage(new TextComponent(ChatColor.RED + "" + ChatColor.STRIKETHROUGH + "-----------------------------------"));
         }
+    }
 
-        // Return if current rotation is suitable for current player count.
-        if (bestCandidate == rotationManager.getRotation() || bestCandidate == null) return;
+    /**
+     * Returns MINI if players < 16
+     * Returns MEDIUM if 16 < players < 40
+     * Returns MEGA if players > 40
+     *
+     * @param players Current participant player count.
+     * @param rotationManager The {@link RotationManager}
+     * @return any of {@link RotationCategory}
+     */
+    private RotationCategory getAppropriateRotationCategory(int players, RotationManager rotationManager) {
+        if (players <= 16 && rotationManager.getRotation("mini") != null) return RotationCategory.MINI;
+        if (players > 16 && players <= 40 && rotationManager.getRotation("medium") != null) return RotationCategory.MEDIUM;
+        if (players > 40 && rotationManager.getRotation("mega") != null) return RotationCategory.MEGA;
 
-        // Change rotation to accommodate players & announce
-        rotationManager.setRotation(bestCandidate);
-        CycleMatchModule cmm = event.getMatch().needMatchModule(CycleMatchModule.class);
-        cmm.startCountdown(cmm.getConfig().countdown());
-
-        event.getMatch().getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.STRIKETHROUGH + "-----------------------------------");
-        event.getMatch().getServer().broadcastMessage(ChatColor.GOLD + "           " + ChatColor.BOLD + "" + new TranslatableComponent("rotation.change.broadcast.title").getTranslate());
-        event.getMatch().getServer().broadcastMessage(ChatColor.YELLOW + "  " + new TranslatableComponent("rotation.change.broadcast.info").getTranslate());
-        event.getMatch().getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.STRIKETHROUGH + "-----------------------------------");
+        return null;
     }
 }
