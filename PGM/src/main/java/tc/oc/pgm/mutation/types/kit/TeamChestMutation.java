261a032ea5afd32f737e033aac261e25910f0aef
Geoff Yoerger <geoffreyiy1@gmail.com>
2018-06-09 13:16:32 -0500

    Organize `onChestUse` and refactor `onInventoryClick`

--- PGM/src/main/java/tc/oc/pgm/mutation/types/kit/TeamChestMutation.java
+++ PGM/src/main/java/tc/oc/pgm/mutation/types/kit/TeamChestMutation.java
@@ -2,6 +2,7 @@ package tc.oc.pgm.mutation.types.kit;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -59,15 +60,16 @@ public class TeamChestMutation extends KitMutation {
     // Open shared inventory instead of placing the chest
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     public void onChestUse(PlayerInteractEvent event) {
-        if (event.getItem() == null) return;
-
-        if (!(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
-        if (event.getItem().getType() != TOOL_TYPE) return;
-
         Player bukkitPlayer = event.getPlayer();
-        if (bukkitPlayer == null) return;
-        Optional<Inventory> oTeamInventory = getTeamsInventory(bukkitPlayer);
+        Optional<MatchPlayer> player = match().participant((Entity) bukkitPlayer);
+        if (!player.isPresent() ||
+                !(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) ||
+                event.getItem() == null ||
+                event.getItem().getType() != TOOL_TYPE) {
+            return;
+        }
 
+        Optional<Inventory> oTeamInventory = getTeamsInventory(bukkitPlayer);
         oTeamInventory.ifPresent(teamInventory -> {
             event.setCancelled(true);
             // If the item is in the off-hand slot, it wont get put back visually for the player without this.
@@ -78,30 +80,30 @@ public class TeamChestMutation extends KitMutation {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onInventoryClick(InventoryClickEvent event) {
+        Player player = event.getActor();
         if (event.getCurrentItem() == null) return;
 
         // No putting blacklisted items (ender chest, possibly wool) into the chest
         Optional<Inventory> teamChest = getTeamsInventory(event.getActor());
-        if (teamChest.filter(teamInventory -> teamInventory.equals(event.getView().getTopInventory())
-        ).isPresent() &&
+        if (teamChest.filter(teamInventory -> teamInventory.equals(event.getView().getTopInventory())).isPresent() &&
                 isBlacklistedItem(event.getCurrentItem())) {
             event.setCancelled(true);
             return;
         }
 
         // If normal right click, in their inventory, on the chest, then open shared inventory.
-        getTeamsInventory(event.getActor()).ifPresent(teamInventory -> {
+        getTeamsInventory(player).ifPresent(teamInventory -> {
             if (event.getInventory().getType() == InventoryType.CRAFTING &&
                     event.getCurrentItem().getType() == TOOL_TYPE &&
                     event.getAction() == InventoryAction.PICKUP_HALF) {
                 event.setCancelled(true);
                 // This resets their mouse position annoyingly, but without it items can get stuck in places.
-                event.getActor().closeInventory();
+                player.closeInventory();
                 // Prevent visual inconsistencies, specifically off-hand slot
                 if (event.getSlotType() == InventoryType.SlotType.QUICKBAR) {
-                    event.getActor().updateInventory();
+                    player.updateInventory();
                 }
-                event.getActor().openInventory(teamInventory);
+                player.openInventory(teamInventory);
             }
         });
     }
